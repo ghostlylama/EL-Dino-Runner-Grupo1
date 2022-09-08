840e89645463cabc41da4e20da24045c28dfe5eb
ghostlylama <edsonlamas14@gmail.com>
2022-09-07 21:56:38 -0400

    Comit_class_3

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,9 @@
 import pygame
+from components.obstacle_manager import ObstacleManager
+from utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from utils import text_utils
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from components.dinisaur import Dinosaur
 
 
 class Game:
@@ -14,6 +17,10 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.dinosaur = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
+        self.points = 0
+        self.game_running = True
 
     def run(self):
         # Game loop: events - update - draw
@@ -22,20 +29,30 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        pygame.quit()
+
+    def execute(self):
+        while self.game_running:
+            if not self.playing:
+                self.show_menu()
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
+                self.game_running = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.dinosaur.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.dinosaur.draw(self.screen)
+        self.show_score()
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -47,3 +64,40 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+    def show_score(self):
+        self.points += 1
+        if self.points % 100 == 0:
+            self.game_speed += 1
+        text, text_rect = text_utils.get_score_element(self.points)
+        self.screen.blit(text, text_rect)
+
+    def show_menu(self):
+        self.game_running = True
+
+        white_color = (255, 255, 255)
+        self.screen.fill(white_color)
+
+        self.show_options_menu()
+
+        pygame.display.update()
+
+        self.handle_key_events_menu()
+
+    def show_options_menu(self):
+        half_screen_height = SCREEN_HEIGHT // 2
+        half_screen_width = SCREEN_WIDTH // 2
+
+        text, text_rect = text_utils.get_text_element('Press any Key to Star', half_screen_width, half_screen_height)
+
+        self.screen.blit(text, text_rect)
+
+    def handle_key_events_menu(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.game_running = False
+                pygame.display.quit()
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                self.run()
